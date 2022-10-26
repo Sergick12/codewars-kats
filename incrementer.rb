fcbe62aa2391cbca1a57ee290b8abb1afb44b0fb
Sergick12 <cepgruddd1@gmail.com>
2022-10-26 04:45:23 +0300

    incrementer-completed

---
+++ incrementer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Incrementer
+  attr_accessor :nums
+
+  def initialize(nums)
+    @nums = nums
+  end
+
+  def call
+    nums.map.each_with_index {|el, index| (el + index + 1) % 10}
+  end
+end
---
+++ spec/incrementer_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require_relative '../incrementer'
+
+RSpec.describe Incrementer do
+  describe 'Task' do
+    context 'Should return array elements' do
+      it { expect(described_class.new([1,2,3]).call).to eq([2,4,6]) }
+    end
+    context 'Should return empty array ' do
+      it { expect(described_class.new([]).call).to eq([]) }
+    end
+  end
+  
+end
