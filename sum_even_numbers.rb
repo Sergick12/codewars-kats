333dc0835a4409b31fb111c2055cb03b208d411b
Sergick12 <cepgruddd1@gmail.com>
2022-10-26 06:08:34 +0300

    sun-even-number-completed

---
+++ sum_even_numbers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class SumEvenNumbers
+  attr_accessor :seq
+
+  def initialize(seq)
+    @seq = seq
+  end
+
+  def call
+    res = 0
+    seq.each { |el| res += el if (el % 2).zero? }
+    res
+  end
+end
