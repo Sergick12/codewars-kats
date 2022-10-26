775ae770903e1149feb38cbe6f5f1c663215bc4b
Sergick12 <cepgruddd1@gmail.com>
2022-10-26 06:52:25 +0300

    all_unique_complete

---
+++ all_unique.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class AllUnique
+  attr_accessor :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def call
+    str.split('').each{|x|  return false if str.count(x) > 1}
+    true
+  end
+end
