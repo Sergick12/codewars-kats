96530e76db3257ec3b267e15da5ee8dcd39557b8
Sergick12 <cepgruddd1@gmail.com>
2022-10-28 17:22:54 +0300

    little fix

--- mumbling.rb
+++ mumbling.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
+# Mumbling
 class Mumbling
-  attr_accessor :s
+  attr_accessor :str
 
-  def initialize(s)
-    @s = s
+  def initialize(str)
+    @str = str
   end
 
   def call
-    b = s.upcase.chars
-    c = s.upcase.chars
-    @s = s.downcase.chars
+    b = str.upcase.chars
+    c = str.upcase.chars
+    @str = str.downcase.chars
     b.map do |x|
       index_char = c.find_index(x)
       c[index_char] = '|'
-
       count = 0
       while count < index_char
-        b[index_char] += s[index_char]
+        b[index_char] += str[index_char]
         count += 1
       end
     end
