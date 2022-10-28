c721c97afed12baf38d880afbaef65676e0c8835
Sergick12 <cepgruddd1@gmail.com>
2022-10-28 17:33:38 +0300

    little fix and add comment

--- sum_of_digits.rb
+++ sum_of_digits.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
+# SumOfDigits
 class SumOfDigits
   attr_accessor :n
 
-  def initialize(n)
-    @n = n
+  def initialize(value)
+    @value = value
   end
 
   def call
     result = 0
-    String(n).split('').map { |i| result += i.to_i }[n.to_s.length - 1]
+    String(n).split('').map { |i| result += i.to_i }[value.to_s.length - 1]
 
-    return SumOfDigits.new(result).call if n.to_s.length > 1
+    return SumOfDigits.new(result).call if value.to_s.length > 1
 
-    n
+    value
   end
 end
