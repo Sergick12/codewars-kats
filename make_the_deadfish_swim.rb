c5830e6a219e83e4599a6d7139d7acb92814c80d
Sergick12 <cepgruddd1@gmail.com>
2022-11-02 20:10:44 +0300

    fix

--- make_the_deadfish_swim.rb
+++ make_the_deadfish_swim.rb
@@ -2,23 +2,30 @@
 
 # MakeTheDeadfishSwim
 class MakeTheDeadfishSwim
-  attr_accessor :data
+  attr_accessor :data, :value
 
   def initialize(data)
     @data = data
+    @value = 0
   end
 
   def call
     arr_res = []
-    value = 0
     data.split('').each do |x|
-      case x
-      when 'i' then value += 1
-      when 'd' then value -= 1
-      when 's' then value **= 2
-      when 'o' then arr_res << value
+      if x == 'o'
+        arr_res << value
+      else
+        increase_value(x)
       end
     end
     arr_res
+  end
+
+  def increase_value(leter)
+    case leter
+    when 'i' then @value += 1
+    when 'd' then @value -= 1
+    when 's' then @value **= 2
+    end
   end
 end
