36c406f6a54c0991652e8cd05f168c3607605f5a
Sergick12 <58103310+Sergick12@users.noreply.github.com>
2023-02-05 19:27:16 +0300

    convert_str_to_float_complete (#3)
    * convert_str_to_float_complete
    
    * add comment and fix test

---
+++ convert_str_to_float.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# ConvertStrToFloat
+class ConvertSrtToFloat
+  attr_accessor :string_array
+
+  def initialize(string_array)
+    @string_array = string_array
+  end
+
+  def call
+    string_array.map(&:to_f)
+  end
+end
