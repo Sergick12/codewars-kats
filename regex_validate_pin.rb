c0863e332fc6265b10ef462fa51d1f705d4d0843
Sergick12 <58103310+Sergick12@users.noreply.github.com>
2023-02-05 19:27:06 +0300

    regex-validate-PIN-complete (#4)
    * regex-validate-PIN-complete
    
    * rename and add comment
    
    * add method f

---
+++ regex_validate_pin.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# RegexValidatePIN
+class RegexValidatePIN
+  attr_accessor :pin
+
+  def initialize(pin)
+    @pin = pin
+  end
+
+  def call
+    (f.zero? ? true : false) &&
+      (pin.length == 4 || pin.length == 6)
+  end
+  def f
+    pin.split('').map { |x| x.to_i.to_s }.join <=> pin
+  end
+end
